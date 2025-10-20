58b356359f2d01a3a7c95ab5572f1797aa0767ac
Jason Liu <jialiu.new@gmail.com>
2025-10-19 23:37:44 -0400

    update import app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LowKeyAI from "./App.jsx";
+import LowKeyAI from "./app.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
